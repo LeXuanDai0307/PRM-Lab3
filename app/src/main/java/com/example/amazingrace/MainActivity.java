506b5cb48fe347d415e6d806a21aa67af5e5082e
VinhNguyen05 <vinhnguyen50501@gmail.com>
2023-05-28 22:38:30 +0700

    change image

--- app/src/main/java/com/example/amazingrace/MainActivity.java
+++ app/src/main/java/com/example/amazingrace/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.amazingrace;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -18,7 +19,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     TextView txtMoney;
     TextView txtBetMoney;
-    EditText etShip1, etShip2, etShip3;
+    EditText etSnail1, etSnail2, etSnail3;
     ImageButton btnStart, btnReset;
     CheckBox cb1, cb2, cb3;
     SeekBar sb1, sb2, sb3;
@@ -33,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
         result = new ArrayList<Integer>();
         setup();
         resetGame();
-
+        MediaPlayer music= MediaPlayer.create(MainActivity.this,R.raw.music);
+        if(!music.isPlaying()){
+            music.setLooping(true);
+            music.start();
+        }
         CountDownTimer countDownTimer;
         countDownTimer = new CountDownTimer(50000, 200) {
             @Override
@@ -68,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 // if all ships finished the race
                 if (sb1.getProgress() >= 100 && sb2.getProgress() >= 100 && sb3.getProgress() >= 100) {
                     this.cancel();
-                    Toast.makeText(MainActivity.this, "Hoàn thành, thuyền về nhất là thuyền số " + result.get(0), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Hoàn thành, ốc sên về nhất là ốc sên số " + result.get(0), Toast.LENGTH_SHORT).show();
                     this.onFinish();
 
                 }
@@ -85,13 +90,13 @@ public class MainActivity extends AppCompatActivity {
                         int betShip3 = 0;
 
                         if (cb1.isChecked()) {
-                            betShip1 = getIntFromEditText(etShip1);
+                            betShip1 = getIntFromEditText(etSnail1);
                         }
                         if (cb2.isChecked()) {
-                            betShip2 = getIntFromEditText(etShip2);
+                            betShip2 = getIntFromEditText(etSnail2);
                         }
                         if (cb3.isChecked()) {
-                            betShip3 = getIntFromEditText(etShip3);
+                            betShip3 = getIntFromEditText(etSnail3);
                         }
 
                         String moneyString = txtMoney.getText().toString().trim();
@@ -143,35 +148,35 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
 
-        int betShip1 = 0;
-        int betShip2 = 0;
-        int betShip3 = 0;
+        int betSnail1 = 0;
+        int betSnail2 = 0;
+        int betSnail3 = 0;
         String moneyString = txtMoney.getText().toString().trim();
         int money = Integer.parseInt(moneyString);
 
         if (cb1.isChecked()) {
-            betShip1 = getIntFromEditText(etShip1);
-            if (betShip1 <= 0 || betShip1 > money) {
-                Toast.makeText(this, "Số tiền đặt cược tàu 1 không hợp lệ!", Toast.LENGTH_SHORT).show();
+            betSnail1 = getIntFromEditText(etSnail1);
+            if (betSnail1 <= 0 || betSnail1 > money) {
+                Toast.makeText(this, "Số tiền đặt cược ốc sên 1 không hợp lệ!", Toast.LENGTH_SHORT).show();
                 return false;
             }
         }
         if (cb2.isChecked()) {
-            betShip2 = getIntFromEditText(etShip2);
-            if (betShip2 <= 0 || betShip2 > money) {
-                Toast.makeText(this, "Số tiền đặt cược tàu 2 không hợp lệ!", Toast.LENGTH_SHORT).show();
+            betSnail2 = getIntFromEditText(etSnail2);
+            if (betSnail2 <= 0 || betSnail2 > money) {
+                Toast.makeText(this, "Số tiền đặt cược ốc sên 2 không hợp lệ!", Toast.LENGTH_SHORT).show();
                 return false;
             }
         }
         if (cb3.isChecked()) {
-            betShip3 = getIntFromEditText(etShip3);
-            if (betShip3 <= 0 || betShip3 > money) {
-                Toast.makeText(this, "Số tiền đặt cược tàu 3  không hợp lệ!", Toast.LENGTH_SHORT).show();
+            betSnail3 = getIntFromEditText(etSnail3);
+            if (betSnail3 <= 0 || betSnail3 > money) {
+                Toast.makeText(this, "Số tiền đặt cược ốc sên 3  không hợp lệ!", Toast.LENGTH_SHORT).show();
                 return false;
             }
         }
 
-        int totalBet = betShip1 + betShip2 + betShip3;
+        int totalBet = betSnail1 + betSnail2 + betSnail3;
         if (money < totalBet) {
             Toast.makeText(this, "Bạn không đủ tiền để đặt cược !", Toast.LENGTH_SHORT).show();
             return false;
@@ -191,9 +196,9 @@ public class MainActivity extends AppCompatActivity {
     private void resetGame() {
         resetMatch();
         txtMoney.setText(String.valueOf(100));
-        etShip1.setText("0");
-        etShip2.setText("0");
-        etShip3.setText("0");
+        etSnail1.setText("0");
+        etSnail2.setText("0");
+        etSnail3.setText("0");
         cb1.setChecked(false);
         cb2.setChecked(false);
         cb3.setChecked(false);
@@ -211,9 +216,9 @@ public class MainActivity extends AppCompatActivity {
         sb2 = (SeekBar) findViewById(R.id.seekBarShip2);
         sb3 = (SeekBar) findViewById(R.id.seekBarShip3);
         txtMoney = (TextView) findViewById(R.id.txtMoney);
-        etShip1 = (EditText) findViewById(R.id.etShip1);
-        etShip2 = (EditText) findViewById(R.id.etShip2);
-        etShip3 = (EditText) findViewById(R.id.etShip3);
+        etSnail1 = (EditText) findViewById(R.id.etShip1);
+        etSnail2 = (EditText) findViewById(R.id.etShip2);
+        etSnail3 = (EditText) findViewById(R.id.etShip3);
         sb1.setEnabled(false);
         sb2.setEnabled(false);
         sb3.setEnabled(false);
